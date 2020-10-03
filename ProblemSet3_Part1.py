056bb8976f1cfd4a06b7ef5797d07ac868e6d8ad
SiqLiang <58503426+SiqLiang@users.noreply.github.com>
2020-10-03 19:53:17 +0800

    task1&2

--- ProblemSet3_Part1.py
+++ ProblemSet3_Part1.py
@@ -1,5 +1,4 @@
-#%%/*-PS3: Code Block 1--*/
-
+#%% task1
 mountain = "Denali"
 nickname = '"Mt. McKinley"'
 elevation = "20322" 
@@ -7,6 +6,23 @@ elevation = "20322"
 print (mountain + ", formerly \nknown as "  + nickname)
 print ("is " + elevation + "' above sea level." )
 
-#%%
+#%% task2
+#open the target file
+dataFolder = "W:\859_data\tri_state"
+
+#Creates a list object called dataList
+dataList = ["roads.shp", "road_types.dbf", "naip_imagery.tif"]
+
+userItem = "streams.shp"
+
+#Adds the userItem string object to the dataList list
+dataList.append(userItem)
+
+for dataString in dataList:
+    print(dataFolder+"\\"+dataString)
+
+#%% 
+
+
 
  
